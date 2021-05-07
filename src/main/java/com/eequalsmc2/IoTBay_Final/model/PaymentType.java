e8088cc2a49a8a18292eb7b45e0d5afd9daa7f55
Xiao ZHONG <zhongxiao0711@gmail.com>
2021-05-07 22:26:50 +0800

    feat: create more tables

---
+++ src/main/java/com/eequalsmc2/IoTBay_Final/model/PaymentType.java
@@ -0,0 +1,9 @@
+package com.eequalsmc2.IoTBay_Final.model;
+
+public enum PaymentType {
+    Paypal,
+    CreditCard,
+    BPay,
+    Alipay,
+    Transfer
+}
---
+++ src/main/java/com/eequalsmc2/IoTBay_Final/model/Status.java
@@ -0,0 +1,6 @@
+package com.eequalsmc2.IoTBay_Final.model;
+
+public enum Status {
+    Pending,
+    Completed
+}
