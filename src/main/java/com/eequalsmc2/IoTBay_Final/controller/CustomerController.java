3a0309b789a6bfe23cca8072e997f1316719dd18
Xiao ZHONG <zhongxiao0711@gmail.com>
2021-05-11 22:04:09 +0800

    refactor: rm unused import

--- src/main/java/com/eequalsmc2/IoTBay_Final/controller/CustomerController.java
+++ src/main/java/com/eequalsmc2/IoTBay_Final/controller/CustomerController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
